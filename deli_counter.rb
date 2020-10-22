277a4fef2a133055ec692686474c8c9f45054043
Arvin Fernandez <arvinf07@gmail.com>
2020-10-22 18:19:43 -0400

    commit tree 80f6c17fc0e94607f052543aceb0c0a600f6f05d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else 
+    new_line = katz_deli.collect.with_index {|name, position| "#{position + 1}. #{name}" }
+    # last = new_line.pop.strip
+    # new_line << last
+    puts "The line is currently: #{new_line.join(' ')}"
+  end  
+end
+
+# Add a new person to the line 
+# Welcome that new person and show them their positon on the line
+
+def take_a_number(katz_deli, person)
+  katz_deli << person
+  puts "Welcome, #{person}. You are number #{katz_deli.size} in line."
+end  
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    customer = katz_deli.shift
+    puts "Currently serving #{customer}."
+  end
+end  
